f95a28e6020b201c880e7dfc55b4a9c016b2152c
Henrick Santiago <henrick.bueno@hotmail.com>
2023-05-12 18:40:09 -0300

    feat: added page info user

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,4 @@
 import { UserContext } from "@/context/user.provider";
-import { User } from "@/@core/domain/entities/user";
 import Link from "next/link";
 import { useContext } from "react";
 import { Registry, container } from "@/@core/infra/container-registry";
@@ -67,11 +66,21 @@ function LogoutButton({ user, signOut }: any) {
 
     return (
         <div className="flex space-x-3">
+            <Link
+                href={"/user"}
+                className="flex text-green"
+            >
+                <h1 className="font-sans text-2xl mr-2 font-bold">{user.name}</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mt-1">
+                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                </svg>
+            </Link>
+            <h1 className="font-sans text-xl font-bold text-grey-700">|</h1>
             <button
                 onClick={signOut}
                 className="flex text-purple items-center"
             >
-                <h1 className="font-sans text-2xl mr-2 font-bold">{user.name}</h1>
+                <h1 className="font-sans text-2xl mr-2 font-bold">Sair</h1>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                     <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z" clipRule="evenodd" />
                 </svg>
---
+++ src/pages/user.tsx
@@ -0,0 +1,14 @@
+
+
+export default function User() {
+
+    return (
+      <main className="flex h-screen flex-col items-center justify-center bg-grey-900">
+        <div className="flex flex-col md:w-3/6">
+          <h1 className="font-sans text-2xl font-bold text-white mb-4">
+            Perfil!
+          </h1>
+        </div>
+      </main> 
+    );
+}
